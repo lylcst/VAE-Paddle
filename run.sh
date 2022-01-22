5c29de94f85273e86014e08600c5a6b8d409daed
liyulin <85541451+lylcst@users.noreply.github.com>
2022-01-22 22:56:30 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-python trainer.py --result_dir result_dir --save_dir checkpoint_dir --mode VAE
+CUDA_VISIBLE_DEVICES=0 \
+python train.py \
+    --result_dir=result \
+    --save_dir=checkpoint \
+    --batch_size=128 \
+    --epoches=100 \
+    --lr=1e-3 \
+    --z_dim=20 \
+    --input_dim=28*28 \
+    --input_channels=1
